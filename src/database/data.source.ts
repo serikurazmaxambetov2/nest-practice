30fe1a5038932301d142b3f0657e3e9d67d9dbc0
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-06-23 18:41:59 +0500

    GPT - приложение для взаимодействия с Chat GPT, с поддержкой контекста.

--- src/database/data.source.ts
+++ src/database/data.source.ts
@@ -4,11 +4,11 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
 
-  host: process.env.POSTGRES_HOST || 'localhost',
+  host: process.env.POSTGRES_HOST,
   port: 5432,
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || '12345678',
-  database: process.env.POSTGRES_DB || 'postgres',
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
 
   entities: [join(__dirname, 'entities', '*.entity.{ts,js}')],
   migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
